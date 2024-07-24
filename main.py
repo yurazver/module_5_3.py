739a704e4fd3cc2a7d619d37d4b77ac0579de636
yurazver <yurazver0@gmail.com>
2024-07-24 22:56:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,29 +5,49 @@ class House:
 
 
     def __eq__(self, other):
-        return self.number_of_floors == other
-
+        if isinstance(other, int):
+            return self.number_of_floors == other
+        else:
+            return self.number_of_floors == other
 
     def __lt__(self, other):
-        return self.number_of_floors < other
+        if isinstance(other, int):
+            return self.number_of_floors < other
+        else:
+            return self.number_of_floors < other
 
     def __le__(self , other):
-        return self.number_of_floors <= other
+        if isinstance(other, int):
+            return self.number_of_floors <= other
+        else:
+            return self.number_of_floors <= other
     def __gt__(self , other):
-        return self.number_of_floors > other
+        if isinstance(other, int):
+            return self.number_of_floors > other
+        else:
+            return self.number_of_floors > other
     def __ge__(self , other):
-        return self.number_of_floors >= other
+        if isinstance(other, int):
+            return self.number_of_floors >= other
+        else:
+            return self.number_of_floors >= other
     def __ne__(self, other):
-        return self.number_of_floors != other
+        if isinstance(other, int):
+            return self.number_of_floors != other
+        else:
+            return self.number_of_floors != other
     def __add__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
     def __iadd__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
     def __radd__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
     def __str__(self):
         return f'Название: {self.name} кол-во этажей:{self.number_of_floors}'
